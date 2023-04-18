936ce7e19c8562e83891c1632c8bbf4c242b779e
AleksanrKotlyar <kotlyar.alek@gmail.com>
2023-04-18 21:47:35 +0300

    fix style btb follow

--- src/components/TweetsList/TweetsList.styled.jsx
+++ src/components/TweetsList/TweetsList.styled.jsx
@@ -100,13 +100,16 @@ export const Followers = styled.p`
 	color: #ebd8ff;
 `;
 export const Btn = styled.button`
-	padding: 14px 56px;
+	/* padding: 14px 56px; */
+	width: 196px;
+	height: 50px;
 	margin-bottom: 36px;
 
 	font-weight: 600;
 	font-size: 18px;
 	text-transform: uppercase;
 	color: #373737;
+	border: none;
 
 	background: #ebd8ff;
 	box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
@@ -115,7 +118,7 @@ export const Btn = styled.button`
 
 	&:hover {
 		background: #37ce97;
-		transform: scale(1.03);
+		transform: scale(1.01);
 	}
 `;
 

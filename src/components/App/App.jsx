f440370eeda06fd93645dd984bf9bd7b01afd109
AleksanrKotlyar <kotlyar.alek@gmail.com>
2023-04-18 21:10:27 +0300

    fix router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,11 @@
 import { lazy } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { Layout } from "components/Layout/Layout";
 import { Toaster } from "react-hot-toast";
 import "utils/index.css";
 
 const HomePage = lazy(() => import("pages/Home"));
 const TweetsPage = lazy(() => import("pages/Tweets"));
-// const NotFoundPage = lazy(() => import("../../pages/NotFoundPage"));
 
 export const App = () => {
 	return (
@@ -15,8 +14,7 @@ export const App = () => {
 				<Route path="/" element={<Layout />}>
 					<Route index element={<HomePage />} />
 					<Route path="/tweets" element={<TweetsPage />} />
-					<Route path="*" element={<HomePage />} />
-					{/* <Route path="*" element={<NotFoundPage />} /> */}
+					<Route path="*" element={<Navigate to="/" />} />
 				</Route>
 			</Routes>
 			<Toaster position="top-right" reverseOrder={false} />
